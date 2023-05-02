49eefbb0e026298ca23d77a9b7c95454955accb7
Saeed Muhammed <36425133+saeedmuhammed@users.noreply.github.com>
2023-05-02 04:33:20 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        hr:"#707070",
+      },
+      fontFamily:{
+        body:["Open+Sans"]
+      }
+    },
+  },
+  plugins: [],
+}
+
